890c17a00cceb0ddabc7882f0ccaa2f173870310
Christian Mueller <chmllr@me.com>
2012-10-17 11:57:38 +0200

    Initial commit

---
+++ src/Rembo/core.clj
@@ -0,0 +1,1 @@
+(ns Rembo.core)
---
+++ test/Rembo/test/core.clj
@@ -0,0 +1,6 @@
+(ns Rembo.test.core
+  (:use [Rembo.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
