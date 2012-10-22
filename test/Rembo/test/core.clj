503f361bda823a358b6d8be41ca0b716d1297a72
Christian Mueller <chmllr@me.com>
2012-10-22 12:14:24 +0200

    tests for user management part of API started

--- test/Rembo/test/core.clj
+++ test/Rembo/test/core.clj
@@ -1,7 +1,33 @@
 (ns Rembo.test.core
-  (:use [Rembo.core])
+  (:use [Rembo.core]
+        [Rembo.persistence])
   (:use [clojure.test]))
 
-(testing "API"
-         (do (user-create "elrodeo" "asdasd" "I'm nothing" "asd@asd.de")
-         ))
+(defn clean-datebase-fixture [f]
+  (flush-database)
+  (f)
+  (flush-database))
+
+(use-fixtures :once clean-datebase-fixture)
+
+
+(deftest persistence
+         (testing "persistence layer"
+                  (is (= nil (retrieve :test)))
+                  (persist :test "lol")
+                  (is (= "lol" (retrieve :test)))
+                  (is (= nil (retrieve :hierarchy :test)))
+                  (persist :hierarchy :test "haha")
+                  (is (= "haha" (retrieve :hierarchy :test)))
+                  (delete :test)
+                  (delete :hierarchy :test)
+                  (is (= nil (retrieve :test)))
+                  (is (= nil (retrieve :hierarchy :test)))))
+
+(deftest user-management
+  (testing "user creation"
+         (user-create "elrodeo" "asdasd" "I'm nothing" "asd@asd.de")
+          (is (= "0" (retrieve :name2id "elrodeo")))
+          (is (= "elrodeo" (retrieve :users "0:name")))
+          (is (= "I'm nothing" (retrieve :users "0:about")))
+          (is (= "asd@asd.de" (retrieve :users "0:email")))))
