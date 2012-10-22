3fa0d0c8a25dbd020f05f07e6eb6c45ff6d52b47
Christian Mueller <chmllr@me.com>
2012-10-22 12:12:51 +0200

    persistence layer implemented

--- src/Rembo/persistence.clj
+++ src/Rembo/persistence.clj
@@ -5,14 +5,42 @@
 ; Initialize the data base 
 (def db 
   (redis/init
-      {:url (get-setting :db-url)}))
+    {:url (get-setting :db-url)}))
 
-; DB hierarchy levels
-(def users "users")
-(def messages "messages")
+; returns the name if the given value is a keyword,
+; or just returns the string back
+(defn- normalize [value]
+  (if (keyword? value) (name value) value))
 
 (defn retrieve 
-  ([key] 0)
-  ([hierarchy key]) )
+  "Retrieves the value of the given field;
+  or retrieves the value of the given field of the specified hash key.
+  If the field name of hash field is a clojure keyword, its name will be used"
+  ([field]
+   (redis/get db (normalize field)))
 
-(defn persist [hierarchy key value] )
+  ([hkey field]
+   (redis/hget db (normalize hkey) (normalize field))))
+
+(defn persist 
+  "Persists the specified value for the given field;
+  or persists the value for the given field of the specified hash key.
+  If the field name of hash field is a clojure keyword, its name will be used"
+  ([field value]
+   (redis/set db (normalize field) value))
+  ([hkey field value]
+   (redis/hset db (normalize hkey) (normalize field) value)))
+
+(defn delete 
+  "Delete the specified value of the given field;
+  or deletes the value for the given field of the specified hash key.
+  If the field name of hash field is a clojure keyword, its name will be used"
+  ([field]
+   (redis/del db [(normalize field)]))
+  ([hkey field]
+   (redis/hdel db (normalize hkey) (normalize field))))
+
+(defn flush-database
+  "Flushes the entire DB (should be used for tests only"
+  []
+  (redis/flush-all db))
